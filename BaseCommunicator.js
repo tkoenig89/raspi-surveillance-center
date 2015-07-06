ed6bca618f20e286c9f99a0abf733dffdad677e3
TK <tobiaskoenig1@googlemail.com>
2015-07-06 19:36:15 +0200

    commenting the code

--- BaseCommunicator.js
+++ BaseCommunicator.js
@@ -7,28 +7,38 @@ var CONST = require("public/constants");
 function Connection(config){
     this.eventListeners = {};
     this.ws = config.ws || null;
+
+    //connect ws to the prototype logic
+    this.ws.on('message', function(message) {
+        this.receive(message);
+    });
 }
 
 Connection.prototype = {
+    /*
+    * Allows registering of eventListers
+    */
     on:function on(eventName,callback){
         var listeners = this.eventListeners[eventName] || (this.eventListeners[eventName] = {});
         listeners.push(callback);
     },
+    
+    /*
+    * handles receiving of messages and forwarding to provided eventListeners
+    */
     receive:function receive(strMsg){
         var msgObj = tryParseJSON(strMsg);
         if (typeof (msgObj) === "object") {
             if(msgObj.isParsedJSON){
                 //properly parsed JSON
-                handleEvent(msgObj.event, connection);
+                handleEvent(msgObj.ev, msgObj.pl);
             }else{
                 //binary data
-                handling[CONST.STATES.BINARY](message, connection);
+                handleEvent(CONST.STATES.BINARY, strMsg);
             }
         } else{
             console.error("String Message: %s", msgObj);
         }
-        
-        var event = params.event;    
 
         /*
         * forwards the received payload and this connection wrapper based on the event
@@ -44,7 +54,7 @@ Connection.prototype = {
     },    
     /*
     * send data to the other party.
-    * can contain payload and or only
+    * can contain payload and or state information only
     */
     send:function send(data,eventName){
         var transmit = {
@@ -55,6 +65,10 @@ Connection.prototype = {
         this.ws.send(sdata);
     }
 }
+
+/*
+* Utility method for json parsing
+*/
 function tryParseJSON(str) {
     var obj = null;
     try {
@@ -65,14 +79,14 @@ function tryParseJSON(str) {
     }
     return obj;
 }
-/*
-* 
-*/
-function _connect(url){
-    var con = new Connection({
-        ws: new WebSocket('wss://' + url, null, { rejectUnauthorized: false })
-    });
 
+/*
+* Connects to a specific url using http or https websockets
+*/
+function _connect(url,secure){
+    var con = new Connection({
+        ws: secure? new WebSocket('wss://' + url, null, { rejectUnauthorized: false }): new WebSocket('ws://'+url)
+    });
 
     return con;
 }

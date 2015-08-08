d94e939ed9898844da0a89ffc59fab82540550c3
maggifix <tobiaskoenig1@googlemail.com>
2015-08-08 18:34:41 +0200

    enhancing the repeater script

--- shell_scripts/repeatImg.sh
+++ shell_scripts/repeatImg.sh
@@ -1,3 +1,8 @@
+FOLDER="/raspi-surveillance-center/shell_scripts"
+STOP_INDICATOR="/tmp/stop"
 
 #run as long as there is no file in /tmp/stop
-while ! test -f /tmp/stop ; do ./shootImage.sh; sleep 15 ; done
+while ! test -f $STOP_INDICATOR ; do sudo $FOLDER/shootImage.sh; sleep 15 ; done
+
+#remove the stop file
+sudo rm $STOP_INDICATOR

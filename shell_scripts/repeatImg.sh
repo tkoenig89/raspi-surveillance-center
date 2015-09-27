ff64c2e9f08ad26c9d630a66d169d0759a990549
TK <tobiaskoenig1@googlemail.com>
2015-09-27 15:21:35 +0200

    script changes
    -scripts support not shooting images in specified hours
    -folder path is now set in repeatImg.sh

--- shell_scripts/repeatImg.sh
+++ shell_scripts/repeatImg.sh
@@ -1,9 +1,16 @@
-FOLDER="/raspi-surveillance-center/shell_scripts"
+SCRIPT_FOLDER="/raspi-surveillance-center/shell_scripts"
 STOP_INDICATOR="/tmp/stop"
+IMG_FOLDER="/imgs"
+HISTORYFOLDER="$IMG_FOLDER/history"
+#set folders to use
+
+#make folder(s) if it doesn't exist
+if ! test -d $IMG_FOLDER ; then sudo mkdir $IMG_FOLDER ; fi
+if ! test -d $HISTORYFOLDER ; then sudo mkdir $HISTORYFOLDER ; fi
 
 #run as long as there is no file in /tmp/stop
 while ! test -f $STOP_INDICATOR ; 
-    do sudo $FOLDER/shootImage.sh; sleep 15 ;
+    do sudo $SCRIPT_FOLDER/shootImage.sh $IMG_FOLDER $HISTORYFOLDER; sleep 15 ;
 done
 
 #remove the stop file
--- shell_scripts/shootImage.sh
+++ shell_scripts/shootImage.sh
@@ -1,22 +1,27 @@
 #sudo raspistill -o /imgs/now.jpg -w 1300 -h 780 -tl 10000 -t 100000
 
 #create a folder for each day
-BASEFOLDER="/imgs"
-HISTORYFOLDER="$BASEFOLDER/history"
+BASEFOLDER=$1
+HISTORYFOLDER=$2
 FOLDER="$HISTORYFOLDER/$(date +%m%m%y)"
 
-#make folder(s) if it doesn't exist
-if ! test -d $BASEFOLDER ; then sudo mkdir $BASEFOLDER ; fi
-if ! test -d $HISTORYFOLDER ; then sudo mkdir $HISTORYFOLDER ; fi
-if ! test -d $FOLDER ; then sudo mkdir $FOLDER ; fi
+#define in which hours of the day there should no image be shot
+NO_IMG_HOURS=";23;24;0;1;2;3;4"
 
-#archive old image
-if ! test -d $BASEFOLDER/now.jpg ; 
-	then sudo cp $BASEFOLDER/now.jpg $FOLDER/img$(date +%H%M%S).jpg ;
+#check if its a hour where no image should be shot
+var="$(expr match "$NO_IMG_HOURS" ".*;$(date +"%H");")";
+if [ $var = 0 ]; then
+
+	#create folder if it doesn't exist
+	if ! test -d $FOLDER ; then
+		sudo mkdir $FOLDER 
+	fi
+
+	#archive old image
+	if test -f $BASEFOLDER/now.jpg ; then
+		sudo cp $BASEFOLDER/now.jpg $FOLDER/img$(date +%H%M%S).jpg
+	fi
+
+	#shoot image: quality 10 seems to be the best result in quality and size
+	sudo raspistill -o $BASEFOLDER/now.jpg -q 10 -e jpg
 fi
-
-#shoot image
-sudo raspistill -o $BASEFOLDER/now.jpg -w 1300 -h 780
-
-#create a small version of the file (width 400 x height accordingly)
-sudo convert $BASEFOLDER/now.jpg -resize 400 $BASEFOLDER/small.jpg
